6d227b22ea6a275a865ef3b004c52ccbb0bbefca
Jonathon Lee <jonnyleealas@gmail.com>
2024-10-21 22:25:36 -0500

    fixed nav bar elements to go far right using fluid and flex

--- frontend/src/components/header/nav.tsx
+++ frontend/src/components/header/nav.tsx
@@ -1,18 +1,25 @@
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import './custom.css'; // Import your custom CSS
 
 function ListExample() {
   return (
-    <Nav defaultActiveKey="/home" as="ul">
-      <Nav.Item as="li">
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Container fluid className='my-nav-container'>
+      <Nav defaultActiveKey="/home" as="ul" className="d-flex justify-content-end w-100 me-5 me-lg-0">
+        <Nav.Item as="li">
+          <Nav.Link href="/home">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Blog</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
   );
 }
 
