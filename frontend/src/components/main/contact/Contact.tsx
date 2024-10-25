73906e43550987a503b283e4fad0343cc913e9c4
Jonathon Lee <jonnyleealas@gmail.com>
2024-10-24 21:30:04 -0500

    added form

--- frontend/src/components/main/contact/Contact.tsx
+++ frontend/src/components/main/contact/Contact.tsx
@@ -1,22 +1,33 @@
 import './contact.css'
 
+
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
-
 
 
 const Contact: React.FC = () => {
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Example textarea</Form.Label>
-                <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-        </Form>
+        <Form className='contact-card'>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     )
 }
 
