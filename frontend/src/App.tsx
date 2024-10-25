ea5a036555b6fac259ea486ca4cbdbe3d6b11bbb
Jonathon Lee <jonnyleealas@gmail.com>
2024-10-24 21:19:13 -0500

    save contact form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Main from './components/Main';
+import Main from './components/main/Main';
 import './App.css'
 
 const App: React.FC = () => {
--- frontend/src/components/main/Main.tsx
+++ frontend/src/components/main/Main.tsx
@@ -1,3 +1,5 @@
+import Contact from './contact/Contact'
+
 type MainProps = {
   className?: string
 };
@@ -66,15 +68,9 @@ const Main: React.FC<MainProps> = ({className}) => {
     <p>More content...</p>
     <p>More content...</p>
     <h1>Main</h1>
-        <h1>Main Content</h1>
-    <p>This is a temporary content for testing the layout.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <p>Additional content to ensure the main section has enough height.</p>
-    <p>More content...</p>
-    <p>More content...</p>
-    <p>More content...</p>
-    <p>More content...</p>
-    <p>More content...</p>
+    <div>
+        <Contact />
+    </div>
 
     </main>
   )
