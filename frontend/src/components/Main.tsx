91470afcba53a14dea47dc1749cf7ae2814028be
Jonathon Lee <jonnyleealas@gmail.com>
2024-10-24 19:29:06 -0500

    added name to nav

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -5,7 +5,7 @@ type MainProps = {
 const Main: React.FC<MainProps> = ({className}) => {
   return (
     <main className={className}>
-        <h1>Main</h1>
+        <h1>Jonathon Lee</h1>
         <h1>Main Content</h1>
     <p>This is a temporary content for testing the layout.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,37 @@
-import Nav from './nav'
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import './custom.css'; // Import your custom CSS
+
 
 type NavbarProps = {
     className?: string
 };
 
-const Navbar: React.FC<NavbarProps> = ({className}) => {
+const Navbar: React.FC<NavbarProps> = ({ className }) => {
     return (
         <nav className={className}>
-            <Nav />
+            <Container fluid className='my-nav-container'>
+                <Nav defaultActiveKey="/home" as="ul" className="d-flex justify-content-between w-100">
+                    <Nav.Item as="li" className="me-auto">
+                        <h1>Jonny</h1>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                        <Nav.Link href="/home">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                        <Nav.Link eventKey="link-1">Projects</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                        <Nav.Link eventKey="link-2">Blog</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                        <Nav.Link eventKey="link-2">Resume</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                        <Nav.Link eventKey="link-2">Contact</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Container>
         </nav>
     )
 }
