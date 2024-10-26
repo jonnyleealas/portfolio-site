a3fd71651fdc0e76650f4a007351e32af837008b
Jonathon Lee <jonnyleealas@gmail.com>
2024-10-26 17:26:33 -0500

    fixed footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -3,10 +3,7 @@ import './footer.css'
 const Footer: React.FC = () => {
   return (
     <footer className='footer'>
-    
         <p>&copy; 2024 MyCompany. All rights reserved.</p>
-       
-      
     </footer>
   );
 };
