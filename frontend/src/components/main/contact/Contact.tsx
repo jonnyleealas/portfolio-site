fe44b20b7b746d6c415aa7738d23dd102ed5dd8a
Jonathon Lee <jonnyleealas@gmail.com>
2024-10-26 20:12:41 -0500

    fixed margin for contact card

--- frontend/src/components/main/contact/Contact.tsx
+++ frontend/src/components/main/contact/Contact.tsx
@@ -1,38 +1,34 @@
-import './contact.css'
-
-
+import './contact.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-
 const Contact: React.FC = () => {
     return (
-        <Form className='contact-card'>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <Button 
-  type="submit" 
-  className="light-grey-button w-100"
->
-  Submit
-</Button>
-      </Form>
-    )
+        <Form className='contact-card form-group '>
+            <h1 className="contact-title mb-4">Contact Me</h1> {/* Add the title here */}
+            
+            <Form.Group className="mb-4" controlId="formBasicEmail">
+              
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="exampleForm.ControlTextarea1">
+                
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+
+            <Button 
+                type="submit" 
+                className="light-grey-button w-100"
+            >
+                Send
+            </Button>
+        </Form>
+    );
 }
 
-export default Contact
-
-
-
+export default Contact;
